6786704149368c039c048774ae726a74a8c23791
Arjun <63303108+ArjunKumarNair@users.noreply.github.com>
2025-08-04 16:13:37 +0800

    Adding health endpoints for liveness and readiness

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ app.post('/', (req, res)=> {
     res.json({chunks});
 });
 
+app.get('/health', (req, res) => {
+    res.json({ status: "UP" });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
